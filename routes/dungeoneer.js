c36940be69b601d5edb1ea621c535bb00f8cd2ad
nightzirch <chisse1049@hotmail.com>
2014-11-25 20:35:23 +0100

    Dungeoneer: added SE and CoF items

--- routes/dungeoneer.js
+++ routes/dungeoneer.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('dungeoneer/index', { title: 'Dungeoneer' });
+	res.render('dungeoneer/index', {
+		title: 'Dungeoneer'
+	});
 });
 
 module.exports = router;
